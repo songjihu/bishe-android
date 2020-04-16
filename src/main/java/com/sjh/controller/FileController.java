ee501879644e8738ec6a8d67026a954df6e5a2f7
sjh <275245802@qq.com>
2020-04-16 16:51:02 +0800

    完成录音上传和播放测试

--- src/main/java/com/sjh/controller/FileController.java
+++ src/main/java/com/sjh/controller/FileController.java
@@ -79,9 +79,33 @@ public class FileController {
     }
 
     @PostMapping(value = "/voiceUpload")
-    public String voiceUpload() {
-        System.out.println("跳转到录音界面");
-        return "voice";
+    public String voiceUpload(@RequestParam(value = "sendVoice") MultipartFile file,
+                              @RequestParam(value = "sendVoice_information")String info,Model model, HttpServletRequest request) {
+        System.out.println("录音文件大小"+file.getSize());
+        System.out.println("录音文件名"+file.getOriginalFilename());
+        File source = null;
+        try {
+            source =multipartFileToFile(file);
+        } catch (Exception e) {
+            System.out.println("转化失败");
+            e.printStackTrace();
+        }
+        String fileName = file.getOriginalFilename();  // 文件名
+        //String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        String filePath = "J://temp-rainy//user_voice//"; // 上传后的路径
+        //fileName = UUID.randomUUID() + suffixName; // 新文件名
+        //fileName=info.split(":")[4];
+        File dest = new File(filePath + fileName);
+        it.sauronsoftware.jave.AudioUtils.amrToMp3(source, dest);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "file";
     }
 
     @RequestMapping("/uploadAudio")
@@ -113,4 +137,37 @@ public class FileController {
             System.out.println("上传失败，因为文件是空的.");
         }
     }
+
+    public static File multipartFileToFile(MultipartFile file) throws Exception {
+
+        File toFile = null;
+        if (file.equals("") || file.getSize() <= 0) {
+            file = null;
+        } else {
+            InputStream ins = null;
+            ins = file.getInputStream();
+            toFile = new File(file.getOriginalFilename());
+            inputStreamToFile(ins, toFile);
+            ins.close();
+        }
+        return toFile;
+    }
+
+    //获取流文件
+    private static void inputStreamToFile(InputStream ins, File file) {
+        try {
+            OutputStream os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[819200];
+            while ((bytesRead = ins.read(buffer, 0, 819200)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            ins.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
